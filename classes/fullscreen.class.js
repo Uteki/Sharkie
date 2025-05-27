58c42a94fda7743d82e24842fdc02d0f5a53277b
Daniel Tran <68958503+Uteki@users.noreply.github.com>
2025-05-27 23:07:31 +0200

    Fullscreen class and link

---
+++ classes/fullscreen.class.js
@@ -0,0 +1,10 @@
+class Fullscreen extends DrawableObject {
+    constructor() {
+        super().loadImage("../assets/content/6.Botones/Full Screen/Mesa de trabajo 7.png");
+
+        this.width = 180;
+        this.height = 40;
+        this.x = 480;
+        this.y = 400;
+    }
+}
