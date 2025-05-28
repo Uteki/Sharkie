97d42593e42578a27bc8d54ac10a37ac5b59909c
Daniel Tran <68958503+Uteki@users.noreply.github.com>
2025-05-28 23:55:55 +0200

    Screen

--- classes/fullscreen.class.js
+++ classes/fullscreen.class.js
@@ -7,4 +7,14 @@ class Fullscreen extends DrawableObject {
         this.x = 480;
         this.y = 400;
     }
+
+    setFullscreen(canvas) {
+        if (canvas.requestFullscreen) {
+            canvas.requestFullscreen();
+        } else if (canvas.webkitRequestFullscreen) {
+            canvas.webkitRequestFullscreen();
+        } else if (canvas.msRequestFullscreen) {
+            canvas.msRequestFullscreen();
+        }
+    }
 }
